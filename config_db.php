144d7685a09d5ef42496e3f0702fba3ff9541718
Newton Olando <juma.olando23@students.dkut.ac.ke>
2026-01-07 10:31:11 +0300

    config_db.php

--- config_db.php
+++ config_db.php
@@ -1,20 +1,19 @@
 <?php
-$host = "dopemusic-server.mysql.database.azure.com";
-$db   = "dopemusic_db";
-$user = "tjedshpbot@dopemusic-server";
-$pass = "Matakomatako@10";
-$port = 3306;
+$host = getenv('DB_HOST');
+$db   = getenv('DB_NAME');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASS');
+$port = getenv('DB_PORT');
 
 try {
     $dsn = "mysql:host=$host;dbname=$db;port=$port;charset=utf8mb4;sslmode=required";
-    
+
     $pdo = new PDO(
         $dsn,
         $user,
         $pass,
         [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
         ]
     );
 } catch (PDOException $e) {
