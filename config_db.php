1b584e1fde4844149184f415850ff21046f818ef
Newton Olando <juma.olando23@students.dkut.ac.ke>
2026-02-26 01:13:36 +0300

    Update config_db.php

--- config_db.php
+++ config_db.php
@@ -1,21 +1,8 @@
-<?php
-$host = "dope.mysql.database.azure.com";
-$db   = "musicdb";
-$user = "dope__alfredo";
-$pass = "Matakomatako@10";   // the one that worked in Cloud Shell
-$port = 3306;
-
-try {
-    $pdo = new PDO(
-        "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4",
-        $user,
-        $pass,
-        [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false
-        ]
-    );
-
-} catch (PDOException $e) {
-    die("DB Connection failed: " . $e->getMessage());
-}
+$pdo = new PDO(
+    "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4",
+    $user,
+    $pass,
+    [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]
+);
